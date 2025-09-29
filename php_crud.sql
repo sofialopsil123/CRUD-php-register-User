a2d60d5a200fa2ea82b973d0b4a343f992e9e5ef
sofialopsil123 <80548975+sofialopsil123@users.noreply.github.com>
2025-09-29 11:34:04 +0200

    Add DB dump (php_crud)

--- php_crud.sql
+++ php_crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-09-2025 a las 11:23:13
+-- Tiempo de generación: 29-09-2025 a las 11:31:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
